fa38dc569222c793ac2fff944cb89b778dacef55
gsr <gsr0003@163.com>
2025-09-28 15:25:36 +0800

    Automated commit message at20250928152536

--- backup.py
+++ backup.py
@@ -1,26 +1,26 @@
 import shutil
 import os
 import time
-import oss2
 import schedule
+import git
 
-def backupLocal(path='backup'):
-    '''if not os.path.exists(backup_path):
-        os.makedirs(backup_path)
-    date_str=time.strftime('%Y%m%d%H%M%S')
-    backup_file=os.path.join(backup_path,f'backup_{date_str}.zip')
-    shutil.make_archive(base_name=backup_file.replace('.zip',''),
-                       format='zip',
-                       root_dir=file_path)
-    return backup_file'''
+def backupLocal(path='../backup'):
     files=[file for file in os.listdir('.') if os.path.isfile(file)]
     if not os.path.exists(path):
         os.makedirs(path)
-    #os.chdir('../')
-    os.chdir(path)
     date=time.strftime('%Y%m%d%H%M%S')
-    os.makedirs(f'{date}')
+    os.makedirs(f'{path}/{date}')
     for file in files:
-        #print(f'{path}/{date}/{file}')
-        shutil.copy(f'../{file}',f'../{path}/{date}/{file}')
-    os.chdir('../')
+        shutil.copy(file,f'{path}/{date}/{file}')
+
+def backupGitHub(path='.'):
+    try:
+        repo=git.Repo(path)
+    except:
+        repo.git.Repo.init(path)
+    repo.git.add('-A')
+    date=time.strftime('%Y%m%d%H%M%S')
+    commit_message=f'Automated commit message at{date}'
+    repo.index.commit(commit_message)
+    origin=repo.remote(name='origin')
+    origin.push()
