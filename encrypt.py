a516efd85a6d77a67abaf6cc7d6bc9cac63d8ce4
Auto Backup <backup@example.com>
2025-10-13 15:56:42 +0800

    Update at 2025.10.13 15:56:42

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,7 @@ from Crypto.Util.Padding import pad
 
 def encrypt3DES(file_path,file_name,key):
     iv=time.strftime('%d%H%M%S')
-    des=pyDes.triple_des(key,pyDes.CBC,iv,pad=None,padmode=pyDes.PAD_PKCS5)
+    des=pyDes.triple_des(key.encode(),pyDes.CBC,iv.encode(),pad=None,padmode=pyDes.PAD_PKCS5)
     with open(file_path+file_name,'rb') as encry_in:
         with open(f'{file_path}3DES_{key}_{file_name}','wb') as encry_out:
             encry_out.write(des.encrypt(encry_in.read()))
@@ -14,7 +14,7 @@ def encrypt3DES(file_path,file_name,key):
 def encryptAES(file_path,file_name,key):
     ctime=time.strftime('%d%H%M%S')
     iv=f'{key[4:12]}{ctime}'
-    aes=AES.new(key,AES.MODE_CBC,iv)
+    aes=AES.new(key.encode(),AES.MODE_CBC,iv.encode())
     with open(file_path+file_name,'rb') as encry_in:
         with open(f'{file_path}AES_{key}_{file_name}','wb') as encry_out:
             encry_out.write(aes.encrypt(pad(encry_in.read(),AES.block_size)))
