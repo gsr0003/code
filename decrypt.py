25f63399e917a5f108caf85a7a6063b3b738b37b
Auto Backup <backup@example.com>
2025-10-13 16:40:19 +0800

    Update at 2025.10.13 16:40:19

--- decrypt.py
+++ decrypt.py
@@ -6,17 +6,17 @@ from Crypto.Util.Padding import pad
 
 def decrypt3DES(file_path,file_name,dst_path):
     key=file_name[5:21]
-    timestamp=os.path.getctime(file_path+file_name)
+    timestamp=os.path.getctime(file_path)
     dt=datetime.fromtimestamp(timestamp)
     iv=dt.strftime('%d%H%M%S')
-    des=pyDes.triple_des(key,pyDes.CBC,iv,pad=None,padmode=pyDes.PAD_PKCS5)
+    des=pyDes.triple_des(key.encode(),pyDes.CBC,iv.encode(),pad=None,padmode=pyDes.PAD_PKCS5)
     with open(file_path+file_name,'rb') as decry_in:
         with open(dst_path+file_name[22:],'wb') as decry_out:
             decry_out.write(des.decrypt(decry_in.read()))
 
 def decryptAES(file_path,file_name,dst_path):
     key=file_name[4:20]
-    timestamp=os.path.getctime(file_path+file_name)
+    timestamp=os.path.getctime(file_path)
     dt=datetime.fromtimestamp(timestamp)
     ctime=dt.strftime('%d%H%M%S')
     iv=f'{key[4:12]}{ctime}'
